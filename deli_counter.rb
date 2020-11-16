8584fd4d9fdeedfa2ef03cc27b021d3ed74d5ee2
sbb3391 <sbb33391@gmail.com>
2020-11-15 19:31:02 -0500

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def line(array)
+    if array.count() == 0
+        puts "The line is currently empty."
+    else
+        msg = "The line is currently:"
+        array.each_with_index do |value, index|
+            msg += " #{index.to_i + 1}. #{value}"
+        end
+        puts msg
+        end
+    end
+
+def take_a_number(array, string)
+    array.push(string)
+    puts "Welcome, #{string}. You are number #{array.length()} in line."
+end
+
+def now_serving(array)
+    if array.length() == 0
+        puts "There is nobody waiting to be served!"
+    else puts "Currently serving #{array[0]}."
+        array.shift
+    end
+end
